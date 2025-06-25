9e2e93e584ddd29743bf88122cc25822130d8108
krishnakumar51 <godkrishnasskal@gmail.com>
2025-06-25 20:22:07 +0530

    folder structure , logging, exception completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Krishna Kumar",
+    author_email="godkrishnasskal@gmail.com",
+    packages=find_packages()
+)
